a31540df23508c991255c0f73ac2b12941f552c3
LitterStudent <66505171+LitterStudent@users.noreply.github.com>
2021-11-19 21:19:15 +0800

    init

--- src/router/index.js
+++ src/router/index.js
@@ -1,27 +1,65 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Layout from '@/layout/index.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
+const constantRoutes = [
   {
-    path: '/',
-    name: 'Home',
-    component: Home
+    path: '/login',
+    component: () => import('@/views/login.vue'),
+    hidden:true
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/404',
+    component:() => import('@/views/404'),
+    hidden: true
+  },
+  {
+    path: '/',
+    component: Layout,
+    children:[
+      {
+        path:'dashboard',
+        name:'Dashboard',
+        component:()=>import('@/views/dashboard/index'),
+        meta:{ title:'Dashboard',icon: 'dashboard'}
+      }
+    ]
+  },
+  {
+    path:'/example',
+    component:Layout,
+    name:'Example',
+    meta:{ title: 'example' ,icon: 'el-icon-s-help' },
+    children:[
+      {
+        path:'table',
+        name:'Table',
+        component:() => import('@/views/Table/index'),
+        meta: {title: 'Table',icon: 'table'}
+      },
+      {
+        path: 'tree',
+        name: 'Tree',
+        component: () => import('@/views/Tree/index'),
+        meta: { title: 'Tree', icon: 'tree' }
+      },
+      {
+        path: 'complex-table',
+        name: 'ComplexTable',
+        component: () => import('@/views/Table/complexTable'),
+        meta: { title: 'complex Table'}
+      }
+    ]
+  },
+
+  // 404 page must be placed at the end !!!
+  { path: '*', component:() => import('@/views/404'), redirect: '/404', hidden: true }
 ]
 
 const router = new VueRouter({
-  routes
+  routes:constantRoutes
 })
 
 export default router
---
+++ src/views/Table/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      This is Table
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
